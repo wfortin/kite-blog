97b9d037839e946fea85467ff6ec4f21752a69f8
William Fortin <william.fortin@dbtlabs.com>
2025-05-22 08:45:39 -0400

    Initial blog setup with astro

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,2 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Wind Chaser Chronicles";
+export const SITE_DESCRIPTION = "A series of spotlights on destinations that combine kiteboarding and remote work.";
