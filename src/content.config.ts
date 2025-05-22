f80d3a0aec6e9b815304df4640ac1db3debe31e3
William Fortin <william.fortin@dbtlabs.com>
2025-05-22 13:55:16 -0400

    About me

--- src/content.config.ts
+++ src/content.config.ts
@@ -7,9 +7,9 @@ const blog = defineCollection({
   // Type-check frontmatter using a schema
   schema: z.object({
     title: z.string(),
-    description: z.string(),
+    description: z.string().optional(),
     // Transform string to Date object
-    pubDate: z.coerce.date(),
+    pubDate: z.coerce.date().optional(),
     updatedDate: z.coerce.date().optional(),
     heroImage: z.string().optional(),
   }),
